1bdfeb9e56f881ae051a1983c8f94758f52c11b4
vero chacon <luzchaconq@gmail.com>
2025-10-30 21:16:13 -0400

    Erasing unused variables

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,15 +1,10 @@
 import logoImage from '../asset/cinelog.png'
 
-interface LogoProps {
-  text: string;
-}
 
-const Logo = ({ text }: LogoProps) => {
+const Logo = () => {
   return (
     <div className="logo">
       <img src={logoImage} alt="cineLog logo" className="logo__image" />
-
-      {/* /** <h1 className="logo__text">{text}</h1>  */}
       <img src="./" alt="" />
     </div>
   );
