a4364765abeefd536744b67896c6c9821b99e2cb
vero chacon <luzchaconq@gmail.com>
2025-11-03 22:06:19 -0400

    Adding basename=/devops to browserRoute

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import { UserProvider } from "./features/user/contexts/UserProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/devops">
       <AuthProvider>
         <MoviesProvider>
           <WatchlistProvider>
